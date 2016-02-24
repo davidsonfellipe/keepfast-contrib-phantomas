cc1537a837f62b527cefe30076f596a34806462c
Davidson Fellipe <email@fellipe.com>
2016-02-24 01:16:36 -0500

    update plataform to platform

--- reference.js
+++ reference.js
@@ -4,7 +4,7 @@ module.exports = {
     'notFound': {
       'label': 'Number of HTTP 404 responses',
       'criteria': [{
-        'plataform': 'all',
+        'platform': 'all',
         'expected': '0',
         'condition': '=',
         'result': true,
@@ -14,7 +14,7 @@ module.exports = {
     'requests': {
       'label': 'Total number of HTTP requests',
       'criteria': [{
-        'plataform': 'all',
+        'platform': 'all',
         'expected': '99',
         'condition': '<',
         'result': true,
@@ -24,7 +24,7 @@ module.exports = {
     'httpsRequests': {
       'label': 'Total number of HTTPS requests',
       'criteria': [{
-        'plataform': 'all',
+        'platform': 'all',
         'expected': '99',
         'condition': '<',
         'result': true,
@@ -34,7 +34,7 @@ module.exports = {
     'bodySize': {
       'label': 'Size of the content of all responses',
       'criteria': [{
-        'plataform': 'all',
+        'platform': 'all',
         'expected': '938159',
         'condition': '<',
         'result': true,
@@ -44,7 +44,7 @@ module.exports = {
     'contentLength': {
       'label': 'Size of the content of all responses',
       'criteria': [{
-        'plataform': 'all',
+        'platform': 'all',
         'expected': '1165716',
         'condition': '<',
         'result': true,
@@ -54,7 +54,7 @@ module.exports = {
     'htmlCount': {
       'label': 'Number of HTML responses',
       'criteria': [{
-        'plataform': 'all',
+        'platform': 'all',
         'expected': '10',
         'condition': '<',
         'result': true,
@@ -64,7 +64,7 @@ module.exports = {
     'htmlSize': {
       'label': 'Size of HTML responses',
       'criteria': [{
-        'plataform': 'all',
+        'platform': 'all',
         'expected': '130824',
         'condition': '<',
         'result': true,
@@ -74,7 +74,7 @@ module.exports = {
     'cssCount': {
       'label': 'Number of CSS responses',
       'criteria': [{
-        'plataform': 'all',
+        'platform': 'all',
         'expected': '8',
         'condition': '<',
         'result': true,
@@ -84,7 +84,7 @@ module.exports = {
     'cssSize': {
       'label': 'Size of CSS responses',
       'criteria': [{
-        'plataform': 'all',
+        'platform': 'all',
         'expected': '76000',
         'condition': '<',
         'result': true,
@@ -94,7 +94,7 @@ module.exports = {
     'jsCount': {
       'label': 'Number of JS responses',
       'criteria': [{
-        'plataform': 'all',
+        'platform': 'all',
         'expected': '22',
         'condition': '<',
         'result': true,
@@ -104,7 +104,7 @@ module.exports = {
     'jsSize': {
       'label': 'Size of JS responses',
       'criteria': [{
-        'plataform': 'all',
+        'platform': 'all',
         'expected': '358000',
         'condition': '<',
         'result': true,
@@ -114,7 +114,7 @@ module.exports = {
     'imageCount': {
       'label': 'Number of image responses',
       'criteria': [{
-        'plataform': 'all',
+        'platform': 'all',
         'expected': '54',
         'condition': '<',
         'result': true,
@@ -124,7 +124,7 @@ module.exports = {
     'imageSize': {
       'label': 'Size of image responses',
       'criteria': [{
-        'plataform': 'all',
+        'platform': 'all',
         'expected': '1420000',
         'condition': '<',
         'result': true,
@@ -134,7 +134,7 @@ module.exports = {
     'webfontCount': {
       'label': 'Number of web font responses',
       'criteria': [{
-        'plataform': 'all',
+        'platform': 'all',
         'expected': '3.1',
         'condition': '<',
         'result': true,
@@ -144,7 +144,7 @@ module.exports = {
     'webfontSize': {
       'label': 'Size of web font responses',
       'criteria': [{
-        'plataform': 'all',
+        'platform': 'all',
         'expected': '120000',
         'condition': '<',
         'result': true,
@@ -155,7 +155,7 @@ module.exports = {
     'otherCount': {
       'label': 'Number of other responses',
       'criteria': [{
-        'plataform': 'all',
+        'platform': 'all',
         'expected': '6',
         'condition': '<',
         'result': true,
@@ -165,7 +165,7 @@ module.exports = {
     'otherSize': {
       'label': 'Size of other responses',
       'criteria': [{
-        'plataform': 'all',
+        'platform': 'all',
         'expected': '7439',
         'condition': '<',
         'result': true,
@@ -176,7 +176,7 @@ module.exports = {
     'DOMelementsCount': {
       'label': 'Total number of HTML element nodes',
       'criteria': [{
-        'plataform': 'all',
+        'platform': 'all',
         'expected': '894',
         'condition': '<',
         'result': true,
@@ -186,7 +186,7 @@ module.exports = {
     'DOMelementMaxDepth': {
       'label': 'Maximum level on nesting of HTML element node',
       'criteria': [{
-        'plataform': 'all',
+        'platform': 'all',
         'expected': '13',
         'condition': '<',
         'result': true,
@@ -196,7 +196,7 @@ module.exports = {
     'imagesWithoutDimensions': {
       'label': 'Number of <img> nodes without both width and height attribute',
       'criteria': [{
-        'plataform': 'all',
+        'platform': 'all',
         'expected': '0',
         'condition': '=',
         'result': true,
@@ -206,7 +206,7 @@ module.exports = {
     'DOMidDuplicated': {
       'label': 'Number of duplicated IDs found in DOM',
       'criteria': [{
-        'plataform': 'all',
+        'platform': 'all',
         'expected': '0',
         'condition': '=',
         'result': true,
@@ -216,7 +216,7 @@ module.exports = {
     'domains': {
       'label': 'Number of domains used to fetch the page',
       'criteria': [{
-        'plataform': 'all',
+        'platform': 'all',
         'expected': '17',
         'condition': '<',
         'result': true,
@@ -226,7 +226,7 @@ module.exports = {
     'maxRequestsPerDomain': {
       'label': 'Maximum number of requests fetched from a single domain',
       'criteria': [{
-        'plataform': 'all',
+        'platform': 'all',
         'expected': '52',
         'condition': '<',
         'result': true,
@@ -236,7 +236,7 @@ module.exports = {
     'assetsNotGzipped': {
       'label': 'Static assets that were not gzipped',
       'criteria': [{
-        'plataform': 'all',
+        'platform': 'all',
         'expected': '0',
         'condition': '=',
         'result': true,
@@ -246,7 +246,7 @@ module.exports = {
     'bodyHTMLSize': {
       'label': 'The size of body tag content (document.body.innerHTML.length)',
       'criteria': [{
-        'plataform': 'all',
+        'platform': 'all',
         'expected': '321684',
         'condition': '<',
         'result': true,
